d2d281c9d941f2a6932feba056f7aff00f2a807d
Raaz <21ketanraaz@gmail.com>
2023-03-05 22:59:10 +0530

    +Button Added/Exploring Widget Class

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-       Expanded(
-         flex: 1,
-         child:  Image(
-         image:AssetImage('images/dice1.png'),
-       ),
-       ),
-        Expanded(
-          flex: 1,
-          child:  Image(
-            image:AssetImage('images/dice1.png'),
+    return Center(
+      child: Row(
+        children: <Widget>[
+         Expanded(
+           flex: 1,
+           child:TextButton(
+             onPressed: (){
+               print('Left button got pressed');
+             },
+               child: Image.asset('images/dice1.png')),
+         ),
+          Expanded(
+            flex: 1,
+            child:TextButton(
+              onPressed: (){
+                print('Right Button Pressed');
+              },
+                child: Image.asset('images/dice1.png'))
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
