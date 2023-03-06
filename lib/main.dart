38c564bb75343a426fe76d2f1ed7dc262706ac86
Raaz <21ketanraaz@gmail.com>
2023-03-06 12:34:28 +0530

    +function changeDiceNumber

--- lib/main.dart
+++ lib/main.dart
@@ -22,31 +22,34 @@ class DicePage extends StatefulWidget {
   @override
   State<DicePage> createState() => _DicePageState();
 }
-
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void changeDiceNumber(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber =Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child:TextButton(
                 onPressed: (){
-                  setState(() {
-                    leftDiceNumber = Random().nextInt(6)+1;
-                  });
+                  changeDiceNumber();
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png')),
           ),
           Expanded(
               child:TextButton(
                   onPressed: (){
-                    setState(() {
-                      rightDiceNumber = Random().nextInt(6)+1;
-                    });
+                    changeDiceNumber();
                   },
                   child: Image.asset('images/dice$rightDiceNumber.png'))
           ),
